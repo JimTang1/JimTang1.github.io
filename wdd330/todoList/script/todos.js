c16afd5f90aa21772edf0dbbf79fe9b037478e6f
JimTang1 <after062111@gmail.com>
2021-02-11 16:12:20 -0700

    update todo list

--- wdd330/todoList/script/todos.js
+++ wdd330/todoList/script/todos.js
@@ -5,9 +5,15 @@ import {
 // empty todo list array
 let todos = [];
 
+
 //check local storage for exiting ToDo item
 if (localStorage.getItem('todos')) {
     todos = JSON.parse(localStorage.getItem("todos"));
+}
+
+//update local stroage
+let storageArray = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 //clean the input content.
@@ -16,42 +22,60 @@ let clean = () => {
 }
 
 //list Check 
-let listCheck=(e)=>{
-    if(e.target.tagName !== 'LI'){
-        e.target.parentElement.innerHTML = "ho"  
-    } else {
-        e.target.innerHTML = 'test';
-    }
-
+let listCheck = (e) => {
+    if (e.target.tagName !== 'LI') {
+        //e.target.parentElement.innerHTML = "ho";
+        const id = Number(e.target.value);
+        const todo = todos.find(todo => todo.Id === id);
+        todo.Completed = !todo.Completed;
+        storageArray();
+        console.log(todo);
+    } 
+    // else {
+    //     e.target.innerHTML = 'test';
+    // }
     // document.querySelector("li").innerHTML = "You click me";
 }
 
 // Create li and in put content to ul
 let setTodoList = () => {
     if (todos.length > 0) {
-        var txt = document.createTextNode("\u00D7");
-        let todoList = document.querySelector('ul');
+        //var txt = document.createTextNode("\u00D7");
+        let todoList = document.querySelector('#toDo');
         todoList.innerHTML = "";
         todos.forEach(
             (todo) => {
                 const li = document.createElement('li');
                 li.id = todo.Id;
-                
                 li.addEventListener('click', listCheck);
-                li.innerHTML = 
-                `<input type = 'checkbox' 
-                value = "${todo.Id}" 
+                li.innerHTML =
+                    `
+                <input type = 'checkbox' 
+                value = "${todo.Id} ${todo.Completed ? "check": ""}" 
                 class = "checked">                        
                 ${todo.Content}`
-
-                
                 todoList.appendChild(li);
+                
+                
+                // const checkboxs = document.querySelectorAll('input[type="checkbox"]');
+                // checkboxs.forEach(
+                //     checkbox => {
+                //         checkbox.addEventListener('change', (e) => {
+                //             const id = Number(e.target.value);
+                //             const todo = todos.find(todo => todo.Id === id);
+                //             todo.Completed = !todo.Completed;
+                //             storageArray();
+                //             console.log(todo);
 
-            //     todoList.innerHTML +=
-            //         `<li id = ${todo.Id}>
-            // <input type = 'checkbox' value = "${todo.Id}" class = "checked" onclick = "listCheck()">                        
-            // ${todo.Content}</li>`;
-            
+                //         });
+                //     }
+                // )
+
+                //     todoList.innerHTML +=
+                //         `<li id = ${todo.Id}>
+                // <input type = 'checkbox' value = "${todo.Id}" class = "checked" onclick = "listCheck()">                        
+                // ${todo.Content}</li>`;
+
             });
     }
 }
@@ -61,7 +85,7 @@ if (localStorage.getItem('todos')) {
     todos = JSON.parse(localStorage.getItem("todos"));
 }
 setTodoList();
-
+//setTodoList(todos.filter())
 
 //document.querySelector(".checked").addEventListener('click', listCheck);
 
@@ -80,11 +104,9 @@ let save = (event) => {
         todos.push(newTodo);
         console.log(todos);
 
-        localStorage.setItem('todos', JSON.stringify(todos));
+        storageArray();
         setTodoList();
         clean();
     }
 }
 button.addEventListener('click', save);
-
-
