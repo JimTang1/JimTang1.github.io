2b60f88106714fb21de322caa9d21532420a9e2c
JimTang <after062111@byui.edu>
2021-02-12 01:49:56 -0700

    update todo

--- wdd330/todoList/script/todos.js
+++ wdd330/todoList/script/todos.js
@@ -9,7 +9,6 @@ let todos = [];
 //check local storage for exiting ToDo item
 if (localStorage.getItem('todos')) {
     todos = JSON.parse(localStorage.getItem("todos"));
-
 }
 
 //update local stroage
@@ -26,49 +25,67 @@ let clean = () => {
 let listCheck = (e) => {
     console.log(e.target.tagName);
     if (e.target.tagName === 'INPUT') {
-        //e.target.parentElement.innerHTML = "ho";
         const id = parseInt(e.target.value);
         const todo = todos.find(todo => todo.Id === id);
-        //console.log(todo);
         todo.Completed = !todo.Completed;
         console.log(todo);
         storageArray();
         location.reload();
     }
-    
+    if(e.target.tagName === "P"){
+        const id = parseInt(e.target.value);
+        const todo = todos.find(todo => todo.Id === id);
+        todos.pop(todo);
+        storageArray();
+        location.reload();
+    }
 }
 
 // Create li and in put content to ul
 let setTodoList = () => {
     if (todos.length > 0) {
-        //var txt = document.createTextNode("\u00D7");
         let todoList = document.querySelector('#toDo');
         todoList.innerHTML = "";
         let finishedList = document.querySelector('#finished');
         finishedList.innerHTML = "";
         todos.forEach(
             (todo) => {
-                const li = document.createElement('li');
-                li.addEventListener('click', listCheck);
-                li.id = todo.Id;
                 if (todo.Completed === false) {
+                    const li = document.createElement('li');
+                    li.addEventListener('click', listCheck);
                     li.id = todo.Id;
                     li.innerHTML =
                         `
-                <input type = 'checkbox' 
-                value = "${todo.Id} ${todo.Completed ? "check": ""}" 
-                class = "checked">                        
-                ${todo.Content}`
+                    <div class = "check">
+                        <input type = 'checkbox' 
+                        value = "${todo.Id} ${todo.Completed ? "check": ""}" 
+                        class = "checked">                        
+                        ${todo.Content}
+                    </div>
+                    <div class = "x">
+                        <p>x</p>
+                    </div>
+                    `
                     todoList.appendChild(li);
                 } else {
+                    const li = document.createElement('li');
+                    li.addEventListener('click', listCheck);
+                    li.id = todo.Id;
                     li.innerHTML =
                         `
-                <input type = 'checkbox' 
-                value = "${todo.Id} ${todo.Completed ? "check": ""}" 
-                class = "checked">                        
-                ${todo.Content}`
+                    <div class = "check">
+                        <input type = 'checkbox' 
+                        value = "${todo.Id} ${todo.Completed ? "check": ""}" 
+                        class = "checked">                        
+                        ${todo.Content}
+                    </div>
+                    <div class = "x">
+                        <p>x</p>
+                    </div>
+                    `
                     finishedList.appendChild(li);
                 }
+                
             });
     }
 
@@ -76,7 +93,6 @@ let setTodoList = () => {
 
 //check local storage for exiting ToDo item
 setTodoList();
-//setTodoList(todos.filter())
 
 // Add Button Even.
 // take content and push to the todos array.
