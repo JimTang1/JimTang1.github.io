2875ff27cfc814efa22b212e8951cbd9ccc8932f
JimTang <after062111@byui.edu>
2021-02-13 00:45:36 -0700

    delete wdd330

--- wdd330/todoList/script/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", (event)=>{
-
-    const cry = document.querySelector('#copyrightyear');
-    cry.textContent = new Date().getFullYear();
-
-    const lu = document.querySelector('#lastupdated');
-    lu.textContent = document.lastModified;
-
-})
