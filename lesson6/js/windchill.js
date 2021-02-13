55c74c35cdeffbd1ed91f3a3164d3dddd9489daf
JimTang <after062111@byui.edu>
2021-02-13 02:12:39 -0700

    lesson 6

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', (e)=>{
+    const high = document.querySelector(".high");
+    const speed = document.querySelector(".speed");
+    const chill = document.querySelector(".chill");
+    const humi = document.querySelector(".humi");
+
+    
+})
