7e2d084e5f4a356edc5d6f8216734d2aae8c3a80
JimTang <after062111@byui.edu>
2021-02-13 22:30:05 -0700

    add web form load and windchill Calculater

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -4,5 +4,12 @@ window.addEventListener('load', (e)=>{
     const chill = document.querySelector(".chill");
     const humi = document.querySelector(".humi");
 
-    
+    speed.innerHTML = 30;
+    high.innerHTML = 40;
+    humi.innerHTML = 50;
+
+    console.log(speed.innerHTML);
+    const windChill = (35.74 +(0.6215 * high.innerHTML) - (3.75 * Math.pow(speed.innerHTML, 0.16)) + 
+                        0.4275 *high.innerHTML * Math.pow(speed.innerHTML, 0.16));
+    chill.innerHTML = Math.floor(windChill);
 })
