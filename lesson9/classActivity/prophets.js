2bdcd6d45ec62b5d468f77a9ba8a747cf417d80a
JimTang1 <after062111@gmail.com>
2021-03-04 09:52:18 -0700

    update foreach for prophets json

--- lesson9/classActivity/prophets.js
+++ lesson9/classActivity/prophets.js
@@ -4,39 +4,51 @@ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-
     .then(result => result.json())
     .then(jsonResult =>{
         console.log(jsonResult);
-        // jsonResult.results.forEach(
-        //     prophet=>{
-        //         let card = document.createElement('section');
-        //         let h2 = document.createElement('h2');
-        //         h2.textContent = prophet.name + ' ' + prophet.lastname;
+        jsonResult.prophets.forEach(
+            prophet=>{
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let birthdate = document.createElement('p');
+                let birthplace = document.createElement('p');
+                let image = document.createElement('img');
+
+                h2.textContent = prophet.name + ' ' + prophet.lastname;
+                birthdate.textContent = prophet.birthdate;
+                birthplace.textContent = prophet.birthplace;
+                image.setAttribute('src', prophet.imageurl);
+                image.setAttribute('alt', prophet.name)
     
-        //         card.appendChild(h2);
-        //         document.querySelector('div.cards').appendChild(card);
-        //     }   
-        // )
+                card.appendChild(h2);
+                card.appendChild(birthdate);
+                card.appendChild(birthplace);
+                card.appendChild(image);
 
-        const prophets = jsonResult['prophets'];
+                document.querySelector('div.cards').appendChild(card);
+            }   
+        )
+
+        // const prophets = jsonResult['prophets'];
         
-        for (let i = 0; i < prophets.length; i++ ) {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let birthdate = document.createElement('p');
-            let birthplace = document.createElement('p');
-            let image = document.createElement('img');
+        // for (let i = 0; i < prophets.length; i++ ) {
+        //     let card = document.createElement('section');
+        //     let h2 = document.createElement('h2');
+        //     let birthdate = document.createElement('p');
+        //     let birthplace = document.createElement('p');
+        //     let image = document.createElement('img');
 
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            birthdate.textContent = prophets[i].birthdate;
-            birthplace.textContent = prophets[i].birthplace;
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', prophets[i].name)
+        //     h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        //     birthdate.textContent = prophets[i].birthdate;
+        //     birthplace.textContent = prophets[i].birthplace;
+        //     image.setAttribute('src', prophets[i].imageurl);
+        //     image.setAttribute('alt', prophets[i].name)
 
-            card.appendChild(h2);
-            card.appendChild(birthdate);
-            card.appendChild(birthplace);
-            card.appendChild(image);
+        //     card.appendChild(h2);
+        //     card.appendChild(birthdate);
+        //     card.appendChild(birthplace);
+        //     card.appendChild(image);
 
-            document.querySelector('div.cards').appendChild(card);
-        }
+        //     document.querySelector('div.cards').appendChild(card);
+        // }
     });
 
