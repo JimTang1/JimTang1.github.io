6493582559202004531c7bcca1c4c755dd27aa3f
JimTang <after062111@byui.edu>
2021-03-12 23:19:33 -0700

    windchill.js update

--- lesson10/classActivity/js/weatherapi.js
+++ lesson10/classActivity/js/weatherapi.js
@@ -1,16 +1,30 @@
 window.addEventListener("load",(e)=>{
-    const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5605242&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial";
-    
+    const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial";
+    const forcaseURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial";
+
     fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-    console.log(jsObject);
-    document.querySelector('#current-temp').textContent = jsObject.main.temp;
-    
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    //document.querySelector('#imagesrc').textContent = imagesrc;  // informational specification only
-    document.querySelector('#icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.querySelector('#icon').setAttribute('alt', desc);  
+      console.log('first jsObject: ');
+      console.log(jsObject);
+
+      document.querySelector('#current-temp').textContent = jsObject.main.temp;
+      document.querySelector(".location").textContent = jsObject.name;  
+      document.querySelector('.speed').textContent = jsObject.wind.speed;
+      document.querySelector('.humi').textContent = jsObject.main.humidity;
+      document.querySelector('.chill').textContent = jsObject.main.temp_min;
+      document.querySelector('.high').textContent = jsObject.main.temp_max;
+      document.querySelector('.description').textContent = jsObject.weather[0].description;
+      const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+      const desc = jsObject.weather[0].description;  // note how we reference the weather array
+      document.querySelector('#icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+      document.querySelector('#icon').setAttribute('alt', desc);  
+    })
+
+    fetch(forcaseURL)
+    .then(response => response.json())
+    .then((jsObject) =>{
+      console.log('Second jsObject: ');
+      console.log(jsObject);
+    })
   });
-} )
--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,20 +1,58 @@
+window.addEventListener("load",(e)=>{
+    const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial";
+    const forcaseURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial";
+    
+    fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+    console.log('first jsObject: ');
+    console.log(jsObject);
+    document.querySelector('.speed').textContent = jsObject.wind.speed;
+    document.querySelector('.humi').textContent = jsObject.main.humidity;
+    document.querySelector('.chill').textContent = jsObject.main.temp_min;
+    document.querySelector('.high').textContent = jsObject.main.temp_max;
+    document.querySelector('.description').textContent = jsObject.weather[0].description;
+  })
 
-let speed = parseInt(document.querySelector(".speed").innerHTML);
-console.log(speed);
+  fetch(forcaseURL)
+  .then(response => response.json())
+  .then((jsObject) =>{
+    console.log('Second jsObject: ');
+    console.log(jsObject);
+    console.log('jsObject list: ');
+    console.log(jsObject['list']);  
 
-window.addEventListener('load', (e)=>{
-    const high = document.querySelector(".high");
-    const speed = document.querySelector(".speed");
-    const chill = document.querySelector(".chill");
-    const humi = document.querySelector(".humi");
+    const lists = jsObject['list'];
+    let day = 1;
 
-    speed.innerHTML = 30;
-    high.innerHTML = 40;
-    humi.innerHTML = 50;
-
-    console.log(speed.innerHTML);
-    const windChill = (35.74 +(0.6215 * high.innerHTML) - (3.75 * Math.pow(speed.innerHTML, 0.16)) + 
-                        0.4275 *high.innerHTML * Math.pow(speed.innerHTML, 0.16));
-    chill.innerHTML = Math.floor(windChill);
-})
+    // document.querySelector('.day1').textContent = jsObject
+    // var str = lists[0].dt_txt;
+    // console.log(str);
+    // str1 = str.substr(str.indexOf(' ') + 1);
+    // console.log(str1);
+    // date = str.substr(0,str.indexOf(' '));
+    // console.log(date);
+    // date = new Date(date).toLocaleString('en-us', {weekday:'long'});
+    // console.log(date);
+    
+    for(let i = 0; i < lists.length;i++){
+      let dayTime = lists[i].dt_txt;
+      date = dayTime.substr(0,dayTime.indexOf(' '));
+      date = new Date(date).toLocaleString('en-us', {weekday:'long'});
+      console.log(date);
+      let time = dayTime.substr(dayTime.indexOf(' ') + 1);
+      console.log(time);
+      if(time == "21:00:00"){
+        document.querySelector('.day'+day).textContent = date;
+        
+        day++;
+      }
+      
+    }
+    // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    // const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    // document.querySelector('#icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    // document.querySelector('#icon').setAttribute('alt', desc);  
+  })
+});
 
