10aafbacf65bde236e38c86c49a4f9ba28e1f5e1
JimTang1 <after062111@gmail.com>
2021-03-16 09:38:11 -0600

    week 11 class activity

---
+++ lesson11/classActivity/js/main.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', (e)=>{
+    const url = "json/temples2.json";
+    fetch(url)
+    .then(result => result.json())
+    .then(jsonResult =>{
+        console.log(jsonResult);
+        buildTempleCard(jsonResult[0]);
+        Object.entries(jsonResult).forEach(([key, temple])=>{
+            //console.log(temple);
+            if(temple.state =="ID")
+                buildTempleCard(temple);
+        });
+
+        // jsonResult.forEach(temple =>{
+        //     console.log(temple);
+        //     buildTempleCard(temple);
+        // })
+    });
+})
+
+function buildTempleCard(temple){
+    //console.log(temple);
+    let card = document.createElement('section');
+    card.classList.add('temple');
+    card.addEventListener('click', detailPage(temple));
+    card.innerHTML =`<h2>${temple.name}</h2> 
+                    <img src = "${temple.imageurl}" alt ="${temple.name}>"
+                    <br>
+                    <p class="fp">First President <b>${temple.presidents[0]} 1st of ${temple.presidents.length}</b></p>
+                    <p class = "lp">Current President <b>${temple.presidents[temple.presidents.length-1]}</b></p>
+                    `;
+    document.querySelector('#temples').appendChild(card);
+}
+
+function detailPage(temple){
+    console.log(temple);
+}
