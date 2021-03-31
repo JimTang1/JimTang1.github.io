b1618e6eadbfc86f9dc91e6df0ef06b504a0bfb3
JimTang <after062111@byui.edu>
2021-03-30 23:19:26 -0600

    update images and js

--- lesson13/js/home.js
+++ lesson13/js/home.js
@@ -62,6 +62,60 @@
 //     })
 // });
 
+window.addEventListener("load", (event)=>{
+
+    const cry = document.querySelector('#copyrightyear');
+    cry.textContent = new Date().getFullYear();
+
+    const lu = document.querySelector('#lastupdated');
+    lu.textContent = document.lastModified;
+
+    const requestURL = 'temple.json';
+    fetch(requestURL)
+    .then(result => result.json())
+    .then(jsonResult =>{
+        jsonResult.Temples.forEach(
+            temple =>{
+                let section = document.createElement('section');
+                let div = document.createElement('div');
+                let div2 = document.createElement('div');
+                let name = document.createElement('h2');
+                let image = document.createElement('img');  
+                let info = document.createElement('p');
+                let hr = document.createElement("hr");
+                let address = document.createElement('p');
+                let state = document.createElement('p');
+                let zip = document.createElement('p');
+
+
+
+                name.textContent = temple.name;
+                image.setAttribute('src',temple.imageurl);
+                image.setAttribute('alt',temple.name);
+                info.textContent = "Information: ";
+                address.textContent = temple.address + "," + temple.city;
+                state.textContent = temple.state + " " + temple.zip;
+
+
+                div2.appendChild(name);
+                div.appendChild(image);
+                div.appendChild(hr);
+                div.appendChild(info);
+                div.appendChild(address);
+                div.appendChild(state);
+
+                section.appendChild(div2);
+                section.appendChild(div);
+                document.querySelector('.content').appendChild(section);
+            }
+        )
+        console.log(jsonResult);
+    })
+
+
+});
+
+
 let getDateNow =() =>{
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"];
